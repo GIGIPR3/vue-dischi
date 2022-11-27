c62015315205a63d340da805caee5ec2ba841e63
GIGIPR3 <gigiprencipe47@gmail.com>
2022-11-27 17:16:43 +0100

    .

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+
+
+    <NavBar />
+
+
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+* {
+  font-family: 'Roboto Condensed', sans-serif;
+  margin: 0%;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+
+#app {
+  height: 100vh;
+  background-color: rgb(30, 45, 59);
+}
+
+
+</style>
